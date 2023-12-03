cd964d1407828396232f66fed5dd7ea39b0b0a9d
geraint <git@enchant.me.uk>
2023-12-03 21:32:36 +0000

    Completed day3

--- day03/day03.py
+++ day03/day03.py
@@ -6,17 +6,21 @@ import re
 with open('input.txt', 'r') as fin:
     lines = fin.read().splitlines()
 
-symgrid  = set()
+sym_set  = set()
+gears    = {}
 
 p_number = re.compile(r'\d+')
 p_symbol = re.compile(r'[^\d\.]')
+p_astrsk = re.compile(r'\*')
 
 row = 0
 for line in lines:
     symbols = p_symbol.finditer(line)
     for symbol in symbols:
-        symgrid.add((row,symbol.start()))
-    n = p_number.finditer(line)
+        sym_set.add((row,symbol.start()))    
+    asterisks = p_astrsk.finditer(line)
+    for asterisk in asterisks:
+        gears[(row,asterisk.start())] = set()
     row = row+1
     
 answer1 = 0
@@ -27,11 +31,22 @@ for line in lines:
         flag = False
         for i in [row-1, row, row+1]:
             for j in range(number.start()-1, number.end()+1):
-                if (i,j) in symgrid:
+                if (i,j) in sym_set:
                     flag = True
+                if (i,j) in gears:
+                    gears[(i,j)].add(int(number.group()))
         if flag:
-            answer1 = answer1 + int(number.group())
+            answer1 = answer1 + int(number.group())        
     row = row+1
-print(answer1)
+print('Answer 1:', answer1)
+
+answer2 = 0
+for k,v in gears.items():
+    if 2 == len(v):
+        x = 1
+        for i in v:
+            x = x*i
+        answer2 = answer2 + x
+print('Answer 2:', answer2)
 
                     
