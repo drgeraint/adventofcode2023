7e49e7ddb3264987f6c1a1d1ff6bbc05803bb19d
geraint <git@enchant.me.uk>
2023-12-04 09:14:45 +0000

    Completed day 4

--- day04/day04.py
+++ day04/day04.py
@@ -4,9 +4,11 @@
 with open('input.txt', 'r') as fin:
     lines = fin.read().splitlines()
 
+cards = {}
 answer1 = 0
 for line in lines:
     card, data = line.split(':')
+    card = int(card.split()[1])
     winners = set()
     numbers = set()
     success = set()
@@ -19,9 +21,27 @@ for line in lines:
         if n in winners:
             success.add(n)
     if len(success) > 0:        
-        score = pow(2, len(success)-1)
+        score1 = pow(2, len(success)-1)
+        score2 = len(success)
     else:
-        score = 0
-    answer1 = answer1 + score
-    #print(card, winners, numbers, success, score)
-print(answer1)
+        score1 = 0
+        score2 = 0
+    answer1 = answer1 + score1
+    cards[card] = {'winners':winners, 'numbers':numbers, 'success':success,
+                   'score1':score1, 'score2':score2}
+    #print(card, winners, numbers, success, score1)
+print('Answer 1:', answer1)
+
+cardlist = [x for x in range(1, len(cards)+1)]
+
+def getcard(cardnum):
+    n = cards[cardnum]['score2']
+    if n > 0:
+        for i in range(cardnum+1, cardnum+n+1):
+            cardlist.append(i)
+
+#print(cardlist)
+for card in cardlist:
+    getcard(card)
+#print(cardlist)
+print('Answer 2:', len(cardlist))
