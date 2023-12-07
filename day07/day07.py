eb9c794a8f10550b75e917b5d4b5be0af90affca
geraint <git@enchant.me.uk>
2023-12-07 12:31:04 +0000

    Completed day 7

--- day07/day07.py
+++ day07/day07.py
@@ -15,33 +15,59 @@ def renumber(hand):
     return hand
 
 def classify(hand):
-    cards = [hand[i] for i in range(0,5)]
-    cards.sort()
-    parts = []
+    cards1 = [hand[i] for i in range(0,5)]
+    cards2 = [hand[i] for i in range(0,5)]
+    cards1.sort()
+    cards2.sort()
+    part1 = []
+    part2 = []
+    njokers = cards2.count('B')
+    while('B') in cards2: cards2.remove('B')
     i = 0
     while i < 5:
-        x = cards.count(cards[i])
-        parts.append(x)
+        x = cards1.count(cards1[i])
+        part1.append(x)
         i = i + x   
-    parts.sort(reverse=True)
-    score = ''
-    for i in range(0, len(parts)):
-        score = score + str(parts[i])
-    score = score.ljust(5, '0')
-    score = score + hand
-    return score
+    i = 0
+    while i < 5-njokers:
+        x = cards2.count(cards2[i])
+        part2.append(x)
+        i = i + x   
+    part1.sort(reverse=True)
+    part2.sort(reverse=True)
+    if part2 == [] :
+        part2 = [0]
+    part2[0] = part2[0] + njokers
+    score1 = ''
+    score2 = ''
+    for i in range(0, len(part1)):
+        score1 = score1 + str(part1[i])
+    for i in range(0, len(part2)):
+        score2 = score2 + str(part2[i])
+    score1 = score1.ljust(5, '0')
+    score2 = score2.ljust(5, '0')
+    #print(hand, score1, score2)
+    return (score1, score2)
         
 hands = []
 for line in lines:
-    hand, bid = line.split()
-    hand = renumber(hand)
-    score = classify(hand)
-    hands.append((score, int(bid)))
+    hand1, bid = line.split()
+    hand1 = renumber(hand1)
+    score = classify(hand1)
+    hand2 = hand1
+    hand2 = hand2.replace('B','0')
+    hands.append((int(bid), score[0]+hand1, score[1]+hand2))
 
-ranks = sorted(hands, key=lambda x: x[0])
-#print(ranks)
-score = 0
+ranks = sorted(hands, key=lambda x: x[1])
+score1 = 0
 for i in range(0, len(ranks)):
-    score = score + (i+1)*ranks[i][1]
+    score1 = score1 + (i+1)*ranks[i][0]
 
-print('Answer 1:', score)
+print('Answer 1:', score1)
+
+ranks = sorted(hands, key=lambda x: x[2])
+score2 = 0
+for i in range(0, len(ranks)):
+    score2 = score2 + (i+1)*ranks[i][0]
+
+print('Answer 2:', score2)
