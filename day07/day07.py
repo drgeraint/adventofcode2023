ace145e4bbb3cf1b86d876c9a5534e210bdfd3b6
geraint <git@enchant.me.uk>
2023-12-07 21:13:51 +0000

    Simplified

--- day07/day07.py
+++ day07/day07.py
@@ -4,31 +4,23 @@
 with open('input.txt', 'r') as fin:
     lines = fin.read().splitlines()
 
-def renumber(hand):
-    hand = hand.replace('A','E')
-    hand = hand.replace('K','D')
-    hand = hand.replace('Q','C')
-    hand = hand.replace('J','B')
-    hand = hand.replace('T','A')
-    return hand
+cardmap = {'A':'E', 'K':'D', 'Q':'C', 'J':'B', 'T':'A'}
 
 def classify(hand, part=1):
     cards = [hand[i] for i in range(0,5)]
     cards.sort()
     parts = []
-    n = 5
     if 2 == part:
         njokers = cards.count('B')
         while('B') in cards: cards.remove('B')
-        n = 5 - njokers
     i = 0
-    while i < n:
+    while i < len(cards):
         x = cards.count(cards[i])
         parts.append(x)
         i = i + x   
     parts.sort(reverse=True)
     if 2 == part:
-        if parts == [] : parts = [0]
+        if [] == parts : parts = [0]
         parts[0] = parts[0] + njokers
     score = ''
     for i in range(0, len(parts)):
@@ -39,23 +31,17 @@ def classify(hand, part=1):
 hands = []
 for line in lines:
     hand, bid = line.split()
-    hand = renumber(hand)
+    for card in cardmap:
+        hand = hand.replace(card, cardmap[card])
     score1 = classify(hand, 1)
     score2 = classify(hand, 2)
-    hand2 = hand
-    hand2 = hand.replace('B','0')
-    hands.append((int(bid), score1+hand, score2+hand2))
+    hands.append((int(bid),
+                  score1+hand,
+                  score2+hand.replace('B','0')))
+for part in (1,2):
+    ranks = sorted(hands, key=lambda x: x[part])
+    score = 0
+    for i in range(0, len(ranks)):
+        score = score + (i+1)*ranks[i][0]
+    print('Answer', part, ':', score)
 
-ranks = sorted(hands, key=lambda x: x[1])
-score1 = 0
-for i in range(0, len(ranks)):
-    score1 = score1 + (i+1)*ranks[i][0]
-
-print('Answer 1:', score1)
-
-ranks = sorted(hands, key=lambda x: x[2])
-score2 = 0
-for i in range(0, len(ranks)):
-    score2 = score2 + (i+1)*ranks[i][0]
-
-print('Answer 2:', score2)
