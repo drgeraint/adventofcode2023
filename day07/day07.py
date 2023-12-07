59359990e99c092fcc517b11c54ee3bad0663678
geraint <git@enchant.me.uk>
2023-12-07 20:05:32 +0000

    Removed unnecessary import and duplication in code

--- day07/day07.py
+++ day07/day07.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python3
 
-import re
-
 #with open('test1.txt', 'r') as fin:
 with open('input.txt', 'r') as fin:
     lines = fin.read().splitlines()
@@ -14,49 +12,39 @@ def renumber(hand):
     hand = hand.replace('T','A')
     return hand
 
-def classify(hand):
-    cards1 = [hand[i] for i in range(0,5)]
-    cards2 = [hand[i] for i in range(0,5)]
-    cards1.sort()
-    cards2.sort()
-    part1 = []
-    part2 = []
-    njokers = cards2.count('B')
-    while('B') in cards2: cards2.remove('B')
+def classify(hand, part=1):
+    cards = [hand[i] for i in range(0,5)]
+    cards.sort()
+    parts = []
+    n = 5
+    if 2 == part:
+        njokers = cards.count('B')
+        while('B') in cards: cards.remove('B')
+        n = 5 - njokers
     i = 0
-    while i < 5:
-        x = cards1.count(cards1[i])
-        part1.append(x)
+    while i < n:
+        x = cards.count(cards[i])
+        parts.append(x)
         i = i + x   
-    i = 0
-    while i < 5-njokers:
-        x = cards2.count(cards2[i])
-        part2.append(x)
-        i = i + x   
-    part1.sort(reverse=True)
-    part2.sort(reverse=True)
-    if part2 == [] :
-        part2 = [0]
-    part2[0] = part2[0] + njokers
-    score1 = ''
-    score2 = ''
-    for i in range(0, len(part1)):
-        score1 = score1 + str(part1[i])
-    for i in range(0, len(part2)):
-        score2 = score2 + str(part2[i])
-    score1 = score1.ljust(5, '0')
-    score2 = score2.ljust(5, '0')
-    #print(hand, score1, score2)
-    return (score1, score2)
+    parts.sort(reverse=True)
+    if 2 == part:
+        if parts == [] : parts = [0]
+        parts[0] = parts[0] + njokers
+    score = ''
+    for i in range(0, len(parts)):
+        score = score + str(parts[i])
+    score = score.ljust(5, '0')
+    return score
         
 hands = []
 for line in lines:
-    hand1, bid = line.split()
-    hand1 = renumber(hand1)
-    score = classify(hand1)
-    hand2 = hand1
-    hand2 = hand2.replace('B','0')
-    hands.append((int(bid), score[0]+hand1, score[1]+hand2))
+    hand, bid = line.split()
+    hand = renumber(hand)
+    score1 = classify(hand, 1)
+    score2 = classify(hand, 2)
+    hand2 = hand
+    hand2 = hand.replace('B','0')
+    hands.append((int(bid), score1+hand, score2+hand2))
 
 ranks = sorted(hands, key=lambda x: x[1])
 score1 = 0
