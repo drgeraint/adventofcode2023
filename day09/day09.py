0c2bce09d3834f6fd28dd75540a4879f53173810
geraint <git@enchant.me.uk>
2023-12-09 14:20:21 +0000

    Consolidated functions for both parts

--- day09/day09.py
+++ day09/day09.py
@@ -4,35 +4,26 @@
 with open('input.txt', 'r') as fin:
     lines = fin.read().splitlines()
 
-def process(elements):
+def process(elements, part=1):
     if any(elements):
         nextline = []
         for i in range(0, len(elements)-1):
             nextline.append(elements[i+1]-elements[i])
-        return elements[-1] + process(nextline)
+        if 1 == part:
+            extrapolation = elements[-1] + process(nextline, 1)
+        elif 2 == part:
+            extrapolation = elements[ 0] - process(nextline, 2)
     else:
-        return 0
+        extrapolation = 0
+    return extrapolation
 
-def process2(elements):
-    if any(elements):
-        nextline = []
-        for i in range(0, len(elements)-1):
-            nextline.append(elements[i+1]-elements[i])
-        return elements[0] - process2(nextline)
-    else:
-        return 0
-    
 answer1 = 0
-for line in lines:
-    elements = line.split()
-    elements = [int(x) for x in elements]
-    answer1 = answer1 + process(elements)
-print('Answer 1:', answer1)
-
 answer2 = 0
 for line in lines:
     elements = line.split()
     elements = [int(x) for x in elements]
-    answer2 = answer2 + process2(elements)
+    answer1 = answer1 + process(elements, 1)
+    answer2 = answer2 + process(elements, 2)
+print('Answer 1:', answer1)
 print('Answer 2:', answer2)
 
