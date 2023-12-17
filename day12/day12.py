69cba2fd4ca1462b956c1018fb27ad0e3cebc4d3
geraint <git@enchant.me.uk>
2023-12-17 12:08:38 +0000

    A very useful and instructive lesson on the power of memoization

--- day12/day12.py
+++ day12/day12.py
@@ -3,61 +3,87 @@
 #filename = 'test1.txt'
 filename = 'input.txt'
 
+PART = 2
+
+import functools
 import re
+
+full_lhs = {}
+full_rhs = {}
+
+regexp = {}
 
 with open(filename, 'r') as fin:
     lines = fin.read().splitlines()
 
-def check_pattern(full_lhs, full_rhs, candidate):
-    p = '^[.]*'
-    for i in range(0, len(full_rhs)-1):
-        p += '[#?]{%d}[.?]+?' % full_rhs[i]
-    p += '[#?]{%d}[.?]*$' % full_rhs[-1]
-    p = re.compile(p)
+@functools.lru_cache(maxsize = None)
+def check_pattern(row, candidate):
+    if row in regexp:
+        p = regexp[row]
+    else:
+        p = '^[.]*'
+        for i in range(0, len(full_rhs[row])-1):
+            p += '[#?]{%d}[.?]+?' % full_rhs[row][i]
+        p += '[#?]{%d}[.?]*$' % full_rhs[row][-1]
+        p = re.compile(p)
+        regexp[row] = p
     q = p.match(candidate)
-    r = p.match(full_lhs)
     if q is None:
-        print('Fail q:', candidate)
+        print('Fail:', row, candidate, full_rhs[row])
         return 0
     else:
-        print(candidate, 'matches')
         return 1
     
-def N(lhs, rhs, full_lhs, full_rhs, candidate):
+@functools.lru_cache(maxsize = None)
+def N(row, lhs, rhs): #, candidate):
     if '#' == lhs[0]:
         return 0
-    candidate += '.'
-    return evaluate(lhs[1:], rhs, full_lhs, full_rhs, candidate)
+    #candidate += '.'
+    return evaluate(row, lhs[1:], rhs) #, candidate)
 
-def Y(lhs, rhs, full_lhs, full_rhs, candidate):
+@functools.lru_cache(maxsize = None)
+def Y(row, lhs, rhs): #, candidate):
     if '.' in lhs[0:rhs[0]]:
         return 0
-    candidate += '#'.rjust(rhs[0],'#')
+    #candidate += '#'.rjust(rhs[0],'#')
     if 1 == len(rhs):
         if '#' in lhs[rhs[0]:]:
             return 0
         else:
-            return check_pattern(full_lhs, full_rhs, candidate)
+            return 1 #check_pattern(row, candidate)
     else:
-        return N(lhs[rhs[0]:], rhs[1:], full_lhs, full_rhs, candidate)
-    
-def evaluate(lhs, rhs, full_lhs, full_rhs, candidate=''):
+        return N(row, lhs[rhs[0]:], rhs[1:]) #, candidate) 
+   
+@functools.lru_cache(maxsize = None)
+def evaluate(row, lhs, rhs): #, candidate=''):
     if len(lhs) < sum(rhs)+len(rhs)-1:
         return 0
     if '.' == lhs[0]:
-        return N(lhs, rhs, full_lhs, full_rhs, candidate)
+        return N(row, lhs, rhs) #, candidate)
     elif '#' == lhs[0]:
-        return Y(lhs, rhs, full_lhs, full_rhs, candidate)
+        return Y(row, lhs, rhs) #, candidate)
     elif '?' == lhs[0]:
-        return (N(lhs, rhs, full_lhs, full_rhs, candidate) +
-                Y(lhs, rhs, full_lhs, full_rhs, candidate))    
+        return (N(row, lhs, rhs) + #, candidate) +
+                Y(row, lhs, rhs)) #, candidate))    
 answer1 = 0
+row = 0
 for line in lines:
     lhs, rhs = line.split()
+    if 2 == PART:
+        unfolded_lhs = lhs
+        unfolded_rhs = rhs
+        for i in range(0,4):
+            unfolded_lhs += '?' + lhs
+            unfolded_rhs += ',' + rhs
+        lhs = unfolded_lhs
+        rhs = unfolded_rhs
     rhs = rhs.split(',')
     rhs = [int(x) for x in rhs]
     print('Evaluating:', lhs, rhs)
-    n = evaluate(lhs, rhs, lhs, rhs, '')
-    print(n, lhs, rhs) 
+    full_lhs[row] = lhs
+    full_rhs[row] = rhs
+    n = evaluate(row, lhs, tuple(rhs)) #, '')
+    print(n, lhs, rhs)
+    row += 1
     answer1 += n
-print('Answer 1:', answer1)
+print('Answer:', answer1)
